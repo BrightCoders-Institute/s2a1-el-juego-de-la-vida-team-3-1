0bd59ce2454061ceb5afbddd5723cdf63a1b32f9
STR1ND3R <jmugres123@gmail.com>
2023-12-20 17:56:59 -0600

    Bug fix
    We fixed the the overlaying of generations in the next_generation method and deleted useless comments to clean up the code
    
    Co-authored-by: Bryanroal <bryanroal27@gmail.com>
    Co-authored-by: AlejandroJer <ajeronimo@ucol.mx>
    Co-authored-by: angel23v <angelvazquezvirgen@gmail.com>

--- main.rb
+++ main.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-# puts "░░ ░░ ░░\n░░ ██ ░░\n██ ░░ ░░"
 class Game
   attr_accessor :width, :height, :flag
 
@@ -26,20 +25,20 @@ class Game
     end
   end
 
-
   def count_neighbours(x, y)
     count = 0
     directions = [-1, 0, 1]
 
     directions.each do |dx|
       directions.each do |dy|
-        next if dx == 0 && dy == 0  # Skip the cell itself
+        next if dx == 0 && dy == 0 # Skip the cell itself
 
-        nx, ny = x + dx, y + dy
+        nx = x + dx
+        ny = y + dy
 
         # Check if the neighbor is within the grid
-        if nx >= 0 && nx < @matrix[0].length && ny >= 0 && ny < @matrix.length
-          count += 1 if @matrix[ny][nx][@flag] == '██'
+        if nx >= 0 && nx < @matrix[0].length && ny >= 0 && ny < @matrix.length && (@matrix[ny][nx][@flag] == '██')
+          count += 1
         end
       end
     end
@@ -50,16 +49,22 @@ class Game
   def next_generation
     for y in 0...@heigth
       for x in 0...@width
-        if @matrix[y][x][0] == '██'
-          @matrix[y][x][@flag.abs()-1] = '░░' if count_neighbours(x, y) < 2
-          @matrix[y][x][@flag.abs()-1] = '░░' if count_neighbours(x, y) > 3
-          @matrix[y][x][@flag.abs()-1] = '██' if count_neighbours(x, y) == 2
-        else
-          @matrix[y][x][@flag.abs()-1] = '██' if count_neighbours(x, y) == 3
+
+        # avoid overlaying
+        @matrix[y][x][@flag.abs - 1] = '░░' if @matrix[y][x][@flag] == '░░'
+
+        if @matrix[y][x][@flag] == '██'
+          @matrix[y][x][@flag.abs - 1] = '░░' if count_neighbours(x, y) < 2
+          @matrix[y][x][@flag.abs - 1] = '░░' if count_neighbours(x, y) > 3
+          @matrix[y][x][@flag.abs - 1] = '██' if count_neighbours(x, y) == 2 || count_neighbours(x, y) == 3
+        elsif count_neighbours(x, y) == 3
+
+          @matrix[y][x][@flag.abs - 1] = '██'
         end
       end
     end
-    @flag == 1 ? @flag = 0 : @flag = 1
+
+    @flag = @flag == 1 ? 0 : 1
   end
 
   def show
@@ -67,17 +72,16 @@ class Game
       i.each do |j|
         print j[@flag]
       end
-      # print i
       print "\n"
     end
   end
 
   def bucle
     loop do
-      next_generation
-      puts ''
       show
+      next_generation
       sleep(1)
+      system('clear')
     end
   end
 end
@@ -89,6 +93,7 @@ a = [
   [2, 3],
   [2, 4]
 ]
+
 b = [
   [2, 2],
   [2, 3],
@@ -96,96 +101,25 @@ b = [
   [1, 4],
   [0, 3]
 ]
-# c = [
-  #   [2, 2],
-  #   [2, 3],
-  #   [2, 4],
-  #   [4, 2],
-  #   [4, 3],
-  #   [4, 4]
-  # ]
-  c = [
-    [2, 2],
-    [2, 3],
-    [2, 4],
-    [3, 2],
-    [3, 4],
-    [4, 2],
-    [4, 3],
-    [4, 4]
-  ]
 
+c = [
+  [2, 2],
+  [2, 3],
+  [2, 4],
+  [3, 2],
+  [3, 4],
+  [4, 2],
+  [4, 3],
+  [4, 4]
+]
 
-  g.create(x = c)
-  g.show
-  g.bucle
-  # g.show
-  # puts ""
+# move structure to the center
 
-  # for i in 1..4
-  #   g.next_generation
-  # end
+c.each do |i|
+  i[0] += 1
+  i[1] += 1
+end
 
-  # g.next_generation
-  # g.flag
-  # puts ""
-  # g.show
-  # # puts ""
-  # g.next_generation
-  # g.flag
-  # puts ''
-  # g.show
-  # # g.count_neighbours(2, 2)
-  # g.next_generation
-  # g.flag
-  # puts ''
-  # g.show
+g.create(c)
 
-
-
-  # LAST VERSION OF count_neighbours METHOD
-  # def count_neighbours(x, y)
-  #   count = 0
-
-  #   # Check left neighbor
-  #   if x - 1 >= 0
-  #     count += 1 if @matrix[y][x - 1][@flag] == '██'
-  #   end
-
-  #   # Check top-left neighbor
-  #   if x - 1 >= 0 && y - 1 >= 0
-  #     count += 1 if @matrix[y - 1][x - 1][@flag] == '██'
-  #   end
-
-  #   # Check top neighbor
-  #   if y - 1 >= 0
-  #     count += 1 if @matrix[y - 1][x][@flag] == '██'
-  #   end
-
-  #   # Check top-right neighbor
-  #   if x + 1 < @matrix[0].length && y - 1 >= 0
-  #     count += 1 if @matrix[y - 1][x + 1][@flag] == '██'
-  #   end
-
-  #   # Check right neighbor
-  #   if x + 1 < @matrix[0].length
-  #     count += 1 if @matrix[y][x + 1][@flag] == '██'
-  #   end
-
-  #   # Check bottom-right neighbor
-  #   if x + 1 < @matrix[0].length && y + 1 < @matrix.length
-  #     count += 1 if @matrix[y + 1][x + 1][@flag] == '██'
-  #   end
-
-  #   # Check bottom neighbor
-  #   if y + 1 < @matrix.length
-  #     count += 1 if @matrix[y + 1][x][@flag] == '██'
-  #   end
-
-  #   # Check bottom-left neighbor
-  #   if x - 1 >= 0 && y + 1 < @matrix.length
-  #     count += 1 if @matrix[y + 1][x - 1][@flag] == '██'
-  #   end
-
-  #   count
-  # end
+g.bucle
